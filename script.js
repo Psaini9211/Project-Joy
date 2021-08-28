c2ee63bb2ca8d61d8d187c20bbd18675b26c2588
Psaini9211 <86410039+Psaini9211@users.noreply.github.com>
2021-08-28 17:19:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -79,9 +79,158 @@ var findTime = setInterval(function () {
     
 })
 
-var item1 = document.getElementById("item1")
+var item = [
+  {
+    name: "Mijia Mijia Electric Shaver S500 Suspension Floating System Digital Display Xiaomi Shaver",
+    Price: 38.20,
+    orignal_price:120.01 ,
+    image: "//img10.joybuy.com/N0/s300x300_jfs/t1/52212/40/15463/69403/5dc571a1Ea46cde1e/eca0d8bd4341580c.jpg.dpg"
+  },
 
-function product_Page() {
-  window.location.href = 'product.html';
+  {
+    name: "Sonic electric toothbrush T100 KR double pointed hair care gums IPX7 waterproof millet electric toothbrush blue Shaver",
+    Price: 11.74,
+    orignal_price:120.00 ,
+    image: "//img11.joybuy.com/N0/s300x300_jfs/t1/73021/5/12435/28043/5d9c54cfE9f81a286/a2f4b1126473c706.jpg.dpg"
+  },
+  {
+    name: "Meter home negative ion portable hair dryer negative ion hair care wind volume quick dry millet hair dryer Shaver",
+    Price: 34.58,
+    orignal_price:120.00 ,
+    image: "//img11.joybuy.com/N0/s300x300_jfs/t1/74944/17/13041/61965/5da5d16eE773b53f4/850017bbb4765efb.jpg.dpg"
+  },
+  {
+    name: "Mijia Xiaomi Handheld Hanging Ironing Machine Steam Iron Ironing Machine Household Portable Sterilization, Shaver",
+    Price: 53.89,
+    orignal_price:999.00 ,
+    image: "//img12.joybuy.com/N0/s300x300_jfs/t1/134124/14/9473/24609/5f577236Ef1912bc6/8db23f0c6e90d21e.jpg.dpg"
+  },
+        {
+    name: "(MIJIA) millet LED smart desk lamp 1S eye protection reading and reading bedroom student bedside folding voice dimming",
+    Price: 69.56,
+    orignal_price:999.00 ,
+    image: "//img10.joybuy.com/N0/s300x300_jfs/t1/68253/3/3200/100884/5d15aaafE88be2641/0545f61235358aac.jpg.dpg"
+  },
+  {
+    name: "Chuangmi Xiaobai Camera 2K Ultra HD Monitor Family Home Wireless Network Camera Indoor Smart Home Remote Video HD Night",
+    Price: 71.12,
+    orignal_price:999.00 ,
+    image: "//img13.joybuy.com/N0/s300x300_jfs/t1/112992/37/670/91503/5e8ec9c1E8ea4ca0c/ebac01e75a318c8d.jpg.dpg"
+  },
+   {
+    name: "Xiaomi original 55W gallium nitride GaN cable charging kit (charger + 6A data cable) fast charging black technology",
+     Price: 38.20,
+    orignal_price:120.00 ,
+    image: "//img11.joybuy.com/N0/s300x300_jfs/t1/151157/10/13075/45681/5fe9b713E78fa8a85/b1809581d6f947fe.jpg.dpg"
+  },
+  {
+    name: "Mijia Mijia Electric Shaver S500 Suspension Floating System Digital Display Xiaomi Shaver",
+    Price: 38.20,
+    orignal_price:120.00 ,
+    image: "//img11.joybuy.com/N0/s300x300_jfs/t1/177780/28/21046/95969/6125dce8E0b8763b1/4012cb660596be15.png"
+  },
+]
+
+
+        const items = JSON.stringify(item);
+        
+        localStorage.setItem("MyItem", items)
+
+        let MyItem = localStorage.getItem("MyItem")
+            
+        MyItem = JSON.parse(MyItem)
+        
+    
+        
+          
+
+  function showProduct() {
+    let Product_list = document.getElementById("Product_list")
+    
+    MyItem.forEach(function (product) {
+
+      //Main Div for product list
+      let div = document.createElement("div");
+     div.classList.add("Product_box")
+
+      
+      //Product Name 
+      let Span_for_name = document.createElement("span")
+      Span_for_name.classList.add( "Product_popup")
+      let p_name = document.createElement("p");
+      p_name.innerText = product.name;
+      p_name.classList.add("Product_Name")
+     
+
+    // add products on localStorage
+      
+      
+    
+      Span_for_name.onclick = function () {
+       
+        addProduct(product)
+         window.location.href = "product.html"
+        
+
+       }
+      
+      
+      
+      //Product Price
+      let p_price = document.createElement("p")
+      p_price.innerText = "US$" + " " + product.Price;
+      p_price.classList.add("Price")
+      Span_for_name.append(p_name)
+      
+
+
+      //Product Image
+      let image = document.createElement("img");
+      image.src = product.image;
+      
+
+      //Product orignal Price
+
+      let product_Orignal_price = document.createElement("p")
+      product_Orignal_price.classList.add("Orignal_price")
+      let product_Orignal_price_span = document.createElement("span")
+      product_Orignal_price_span.classList.add("decor")
+      product_Orignal_price_span.innerHTML =  "US$" + " " + product.orignal_price +" " 
+      
+      
+
+      // Product discount
+      
+      let discount_span = document.createElement("span")
+      discount_span.innerHTML = "|" + " " +"(68% off)"
+
+
+
+      
+      product_Orignal_price.append(product_Orignal_price_span, discount_span)
+      div.append(image, Span_for_name ,p_price, product_Orignal_price)
+
+         Product_list.append(div)
+    });
+
+    }
+
+
+showProduct();
+
+
+
+
+  
+  localStorage.setItem("product_page", JSON.stringify([]))
+
+
+
+function addProduct(p) {
+ let Product_page_data = JSON.parse(localStorage.getItem("product_page"))
+
+  Product_page_data.push(p)
+
+  localStorage.setItem("product_page", JSON.stringify(Product_page_data))
+
 }
-item1.addEventListener("click", product_Page)
